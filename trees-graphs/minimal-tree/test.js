a802d9faf8e5478bd47113b93331a953d9ba274e
Rafael Espinoza <nerddiffer@gmail.com>
2017-03-06 15:16:52 -0800

    Minimal tree: Add test for simple binary search tree
    For a perfectly-balanced binary search tree w/ consecutive values.

---
+++ trees-graphs/minimal-tree/test.js
@@ -0,0 +1,33 @@
+const test = require('ava');
+const { BinaryTree, El } = require('../binary-tree.js');
+const { minimizeHeight } = require('./minimize-height.js');
+
+test('#minimizeHeight: works with simple tree with consecutive values', t => {
+  const tree = new BinaryTree();
+  tree.root = new El(8);
+
+  // 1st level
+  tree.root.left = new El(4);
+  tree.root.right = new El(12);
+
+  // 2nd level
+  tree.root.left.left = new El(2);
+  tree.root.left.right = new El(6);
+  tree.root.right.left = new El(10);
+  tree.root.right.right = new El(14);
+
+  // 3rd level
+  tree.root.left.left.left = new El(1);
+  tree.root.left.left.right = new El(3);
+  tree.root.left.right.left = new El(5);
+  tree.root.left.right.right = new El(7);
+  tree.root.right.left.left = new El(9);
+  tree.root.right.left.right = new El(11);
+  tree.root.right.right.left = new El(13);
+  tree.root.right.right.right = new El(15);
+
+  const input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+  const actual = minimizeHeight(input);
+  const expected = tree;
+  t.deepEqual(actual, expected);
+});
