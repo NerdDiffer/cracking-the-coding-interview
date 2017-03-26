044c8484d4f248fe54ea422805a209874cc463d3
Rafael Espinoza <nerddiffer@gmail.com>
2017-03-25 22:32:53 -0700

    Paths with sum: Rewrite count sum paths

--- trees-graphs/paths-with-sum/count-sum-paths.js
+++ trees-graphs/paths-with-sum/count-sum-paths.js
@@ -6,35 +6,31 @@
  * only from parent nodes to child nodes)
  */
 
-const sumPath = (nodeValues, targetSum, cb) => {
-  const len = nodeValues.length;
+const countSumPaths = (tree, targetSum) => {
+  const sumsToCounts = {};
+  return traverse(tree.root, 0)
 
-  for (let end = len - 1; end >= 0; end -= 1) {
-    for (let start = end; start >= 0; start -= 1) {
-      const slice = nodeValues.slice(start, end + 1);
-      const sum = slice.reduce((s, v) => s + v, 0);
-      if (sum === targetSum) { cb(); }
-    }
+  function traverse(node, prevSum) {
+    if (!node) { return 0; }
+
+    const currSum = prevSum + node.value;
+    const diff = currSum - targetSum;
+
+    let total = sumsToCounts.hasOwnProperty(diff) ? sumsToCounts[diff] : 0;
+    if (currSum === targetSum) { total += 1; }
+
+    // add to map
+    if (!sumsToCounts.hasOwnProperty(currSum)) { sumsToCounts[currSum] = 0; }
+    sumsToCounts[currSum] += 1;
+
+    total += traverse(node.left, currSum);
+    total += traverse(node.right, currSum);
+
+    // remove from map
+    sumsToCounts[currSum] -= 1;
+
+    return total;
   }
 };
 
-const countSumPaths = (tree, targetSum) => {
-  let count = 0;
-  const incrementCount = () => count += 1;
-
-  // use DFS to explore paths from root to every leaf node
-  const traverse = (node, path, count) => {
-    if (!node.left && !node.right) {
-      sumPath(path.concat(node.value), targetSum, incrementCount);
-      return;
-    }
-
-    if (node.left) { traverse(node.left, path.concat(node.value), count); }
-    if (node.right) { traverse(node.right, path.concat(node.value), count); }
-  };
-
-  traverse(tree.root, [], count);
-  return count;
-};
-
-module.exports = { sumPath, countSumPaths };
+module.exports = countSumPaths;
